27fafceedd8b348171625b2144e6300863a6e845
백재우 <9712jw@gmail.com>
2024-11-18 02:51:03 +0900

    refactor : 안쓰는 import 제거, 에러 메시지 추가

--- java-convenience-store-7-main/src/main/java/store/common/ErrorMessage.java
+++ java-convenience-store-7-main/src/main/java/store/common/ErrorMessage.java
@@ -2,6 +2,8 @@ package store.common;
 
 public enum ErrorMessage {
     INSUFFICIENT_INVENTORY_NUMBER_PURCHASED("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해주세요."),
+    QUANTITY_IS_LESS_THEN_ZERO("재고는 0 미만으로 감소할 수 없습니다."),
+    PROMOTION_TYPE_INFO_SHOULD_NOT_NULL("프로모션 타입 정보는 필수정보입니다."),
     ;
 
     private final String message;
