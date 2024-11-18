1e1a12677d9943621206244c2473a6a715ef1bc1
백재우 <9712jw@gmail.com>
2024-11-18 14:53:12 +0900

    feat : Product 기능 추가

--- java-convenience-store-7-main/src/main/java/store/domain/product/Product.java
+++ java-convenience-store-7-main/src/main/java/store/domain/product/Product.java
@@ -5,16 +5,50 @@ import store.common.QuantityCounter;
 public class Product {
 
     private final ProductInfo productInfo;
-    private final Quantity promotionQuantity;
+    private final Quantity quantity;
     private final PromotionType promotionType;
 
-    public Product(ProductInfo productInfo, Quantity promotionQuantity, PromotionType promotionType) {
+    public Product(ProductInfo productInfo, Quantity quantity, PromotionType promotionType) {
         this.productInfo = productInfo;
-        this.promotionQuantity = promotionQuantity;
+        this.quantity = quantity;
         this.promotionType = promotionType;
     }
 
     public void reduceProductQuantity(QuantityCounter quantityCounter) {
-        promotionQuantity.reduceOrderQuantity(quantityCounter);
+        quantity.reduceOrderQuantity(quantityCounter);
+    }
+
+    // 프로모션 상품에 추가 제공받을 수 있는지 여부를 검사한다.
+    public boolean inspectionCanGetAdditionalPromotionProduct(QuantityCounter orderQuantity) {
+        return promotionType.inspectionCanGetAdditionalPromotionProduct(orderQuantity);
+    }
+
+    // 프로모션 재고가 부족하여 일부 수량을 프로모션 혜택 없이 결제해야 하는 개수를 계산한다.
+    public int calculatePaidWithoutPromotionQuantity(QuantityCounter orderQuantity) {
+        int givenProduct = promotionType.getAdditionalPromotionProduct(quantity.getPromotionQuantityCounter());
+        int buyGet = promotionType.getPromotionBuyGet();
+        return orderQuantity.calculateMinusQuantityCount(new QuantityCounter(givenProduct * buyGet));
+    }
+
+    // 프로모션 재고가 주문 재고를 처리할 수 있는지 검사한다.
+    public boolean checkPromotionQuantityCanProcess(QuantityCounter orderQuantity) {
+        return quantity.checkPromotionQuantityCanProcess(orderQuantity);
+    }
+
+    // 추가 제공받는 상품의 수를 계산
+    public int getAdditionalPromotionProductQuantity(QuantityCounter orderQuantity) {
+        return promotionType.getAdditionalPromotionProduct(orderQuantity);
+    }
+
+    public ProductInfo getProductInfo() {
+        return productInfo;
+    }
+
+    public Quantity getQuantity() {
+        return quantity;
+    }
+
+    public PromotionType getPromotionType() {
+        return promotionType;
     }
 }
